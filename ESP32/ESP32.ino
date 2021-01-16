0603cc07c57daf51e14cad102c17d3a74832b3b5
Hien Van To <69282968+hientv1999@users.noreply.github.com>
2021-01-15 20:47:21 -0800

    Update ESP32.ino

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -3,7 +3,6 @@ unsigned int DeviceOrder = 1;
 #include "functions.hpp"
 #include <soc/soc.h>
 #include <soc/rtc_cntl_reg.h>
-int button1{25}, button2{26}, button3{27}, button4{14};
 enum St
     {
         WORKING, 
@@ -59,18 +58,6 @@ int EEPROM_usage = 0;                                     //number of BluetoothI
 int EEPROM_uploaded = 0;                                  //address of EEPROM has been uploaded to LAMP server
 String close_contact[682];  
 
-
-//-----------------------------------------------------------------------------------------------------------------------------------------
-//-----------------the section below contains the libraries and global variables to display the SSD1306 OLED Display-----------------------
-//-----------------------------------------------------------------------------------------------------------------------------------------
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <Fonts/FreeMonoBold9pt7b.h>
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 //-----------------------------------------------------------------------------------------------------------------------------------------
 #include "WiFi.h"                                         //Enables the ESP32 to connect to the local network (via WiFi)
 long long int scanTimer;
@@ -91,11 +78,6 @@ void setup()
                                                           //4093 for 1 wifi name saved: 0 if no wifi, otherwise equal number of character                                                    
                                                           //4094 for 1 wifi pass saved: 0 if no wifi, otherwise equal number of character 
                                                           //4095 for 1 auth saved: 0 or 1
-    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
-      Serial.println(F("SSD1306 allocation failed"));
-      for(;;);
-    }
-    welcome_screen();
     BLEDevice::init("MyESP");                                                    //initialzie the local BLE environment as COVID_Alert_Device client device
     BLEServer *pServer = BLEDevice::createServer();                                 //create server
     BLEService *pService = pServer->createService(SERVICE_UUID);
@@ -126,7 +108,6 @@ void setup()
 void loop() {
   switch (state){
     case WORKING:
-        print_info("Dr.ESP32 is working  normally", true, 0);
         if (EEPROM_usage > 440){
             state = FULLMEM;
         } else {
@@ -187,14 +168,12 @@ void loop() {
     
     case FULLMEM:
       if (EEPROM_uploaded == EEPROM_usage){                     //if EEPROM full and no data is missed, back to working now
-          print_info("EEPROM full. All datais uploaded. Switch  to working now", true, 2000);
           Write_Unsigned_Int_Into_EEPROM(3960, 0);                       //Clear memory for number of EEPROM already used
           Write_Unsigned_Int_Into_EEPROM(3962, 0);                       //Clear memory for number of EEPROM already uploaded
           EEPROM.write(3964, WORKING);                          //move to working state after reset
           EEPROM.commit();
           ESP.restart();
       } else {
-          print_info("Switch to upload only mode", true, 2000);
           state = UPLOAD_ONLY;
       }
     break;
@@ -205,7 +184,6 @@ void loop() {
           max_value = EEPROM_usage ;         
       }                                                                      
       if (reconnectWiFi()){                                                 //good connection to WiFi
-            print_info("Dr.ESP32 is uploading only", true, 0);
             for (int k= EEPROM_uploaded ; k < max_value; k++){
                 unsigned int ToDevice = Read_Unsigned_Int_From_EEPROM(9*k);
                 String contact_time = Read_Time_From_EEPROM(9*k+2);
@@ -214,13 +192,11 @@ void loop() {
                 }
             }
         } else {                                                             //lose connection to WiFi
-          print_info("Cannot connect to WiFi ", true, 0);
         }
         Write_Unsigned_Int_Into_EEPROM(3960, EEPROM_usage);
         Write_Unsigned_Int_Into_EEPROM(3962, EEPROM_uploaded); 
         Serial.println(" ");
         if (EEPROM_usage == EEPROM_uploaded){
-          print_info("Uploaded completely!", true, 1000);
           Write_Unsigned_Int_Into_EEPROM(3960, 0);                       //Clear memory for number of EEPROM already used
           Write_Unsigned_Int_Into_EEPROM(3962, 0);                       //Clear memory for number of EEPROM already uploaded
           state = WORKING;
