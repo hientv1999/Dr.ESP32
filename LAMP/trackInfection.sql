d7d9ee2a3f3a5d70373956b9e3d552e0fd5df4e2
Hien Van To <69282968+hientv1999@users.noreply.github.com>
2021-01-15 21:27:50 -0800

    Add files via upload

---
+++ LAMP/trackInfection.sql
@@ -0,0 +1,6 @@
+CREATE TABLE trackInfection (
+    id INT(16) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL,
+    gender VARCHAR(6) NOT NULL,
+    contact_time VARCHAR(19)
+)
